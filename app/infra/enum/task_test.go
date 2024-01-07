5ce129a0e6084729163a6127278160c9fd791d3a
James <jame31560@gmail.com>
2024-01-07 18:04:18 +0800

    feat: enum test

---
+++ app/infra/enum/task_test.go
@@ -0,0 +1,31 @@
+package enum
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToTaskStatusEnum(t *testing.T) {
+	caseList := []struct {
+		input    int8
+		hasErr   bool
+		expected TaskStatusEnum
+	}{
+		{input: int8(TaskStatusNone), hasErr: true},
+		{input: int8(TaskStatusIncomplete), expected: TaskStatusIncomplete},
+		{input: int8(TaskStatusEnd), hasErr: true},
+		{input: -1, hasErr: true},
+	}
+
+	for _, testCase := range caseList {
+		result, err := ToTaskStatusEnum(testCase.input)
+
+		if testCase.hasErr {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+      assert.EqualValues(t, testCase.expected, result)
+		}
+	}
+}
