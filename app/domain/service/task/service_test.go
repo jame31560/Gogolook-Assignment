30ff03c842ef6f6e51c24c2d8f5b8444cda5c8ad
James <jame31560@gmail.com>
2024-01-07 17:19:25 +0800

    feat: service test

---
+++ app/domain/service/task/service_test.go
@@ -0,0 +1,28 @@
+package task
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/google/uuid"
+)
+
+func TestCheckName(t *testing.T) {
+	taskSvc := NewTaskService()
+
+  // 空白名稱
+	result := taskSvc.CheckName("")
+	assert.False(t, result)
+
+  // 正確名稱
+	result = taskSvc.CheckName("Name")
+	assert.True(t, result)
+}
+
+func TestNewTaskID(t *testing.T) {
+	taskSvc := NewTaskService()
+
+	taskID := taskSvc.NewTaskID()
+	_, err := uuid.Parse(taskID)
+	assert.NoError(t, err)
+}
